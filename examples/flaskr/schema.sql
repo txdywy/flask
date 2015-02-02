5d253e82508800be5259f2501d8d92f9f3922b15
weiyi <weiyi@papayamobile.com>
2015-02-02 15:51:44 +0800

    add tables
    hehe

--- examples/flaskr/schema.sql
+++ examples/flaskr/schema.sql
@@ -4,3 +4,18 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+create table users (
+  id integer primary key autoincrement,
+  email text not null,
+  password text not null,
+  name text,
+  icon text
+);
+
+create table projects (
+  id integer primary key autoincrement,
+  name text not null,
+  image text,
+  url text
+);
