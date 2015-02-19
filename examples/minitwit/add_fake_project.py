bf70cfdaf51b3c8cffb9086870c73b31725afdaf
Wei Yi <genius.ron@gmail.com>
2015-02-19 17:56:32 +0000

    fix fake project adding bug;fix project pop up bug

--- examples/minitwit/add_fake_project.py
+++ examples/minitwit/add_fake_project.py
@@ -1,4 +1,4 @@
-from model import *
+from minitwit import *
 from faker import Factory
 fake = Factory.create('en_US')
 add_project(title=str(fake.company()), email=str(fake.email()), desp=str(fake.text()), client=str(fake.name()), image_url='', service='web dev')
