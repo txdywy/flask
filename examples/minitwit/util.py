fa184558d1faad2808fa797b1e4b15e73a0957cd
Wei Yi <genius.ron@gmail.com>
2015-02-20 05:06:28 +0000

    fix unicode to utf8

--- examples/minitwit/util.py
+++ examples/minitwit/util.py
@@ -3,11 +3,23 @@ import email.utils
 from email.mime.text import MIMEText
 ALANCER_SUPPORT = 'support@alancer.cf'
 
+def makes(s):
+    if type(s)==unicode:
+        return s.encode('utf8','ignore')
+    else:
+        return s
+
+def makeu(s):
+    if type(s)==str:
+        return s.decode('utf8','ignore')
+    else:
+        return s
+
 def send_email(title, content, addr_to, addr_fr=None):
     # Create the message
     if not addr_fr:
         addr_fr = ALANCER_SUPPORT   
-    msg = MIMEText(content)
+    msg = MIMEText(makes(content))
     msg['To'] = email.utils.formataddr(('Recipient', addr_to))
     msg['From'] = email.utils.formataddr(('Alancer', addr_fr))
     msg['Subject'] = title
