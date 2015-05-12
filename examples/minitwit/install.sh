3cf1840cb6ba32804189d24fd2e21bdb38d4c73c
Sushi <txdywyjp@outlook.com>
2015-05-12 05:49:39 +0000

    fix install.sh

--- examples/minitwit/install.sh
+++ examples/minitwit/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+if [ -f /usr/local/bin/pip ] || [ -f /usr/bin/pip ];
+then
+    echo "pip exists"
+else
+    echo "Installing pip"
+    wget https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+fi
 sudo apt-get install gcc g++ python-dev postfix mysql-python sqlite
 sudo pip install fabric
 python deploy.py
