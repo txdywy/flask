8e627a2d8faa4e7116a8f4fab652c073a51db793
Yi Wei <genius.ron@gmail.com>
2015-05-12 14:17:45 +0800

    merge fab

--- examples/minitwit/fabfile.py
+++ examples/minitwit/fabfile.py
@@ -1,24 +1,29 @@
 from fabric.api import * 
 env.use_ssh_config = True
 #env.hosts = ['sushi', 'polo', 'noodle']
-def sz(file):
+def send(file):
     put(file, '~/')
 
-def rz(file):
+def recv(file):
     get(file, './')
-
-def deploy_alancer():
-    with cd('./flask/examples/minitwit/'):
-	    run('git pull')
-	    run('supervisorctl restart all')
-
-def update_db():
-    with cd('./flask/examples/minitwit/'):
-        run('git pull')
-        run('openssl enc -des -d -a -in alancer_db_enc -out alancer.db')
 
 def alancer():
     env.hosts = ['sushi', 'polo', 'noodle']
+
+def pull():
+    with cd('~/flask/examples/minitwit/'):
+        run('git pull')
+
+def restart():
+    with cd('~/flask/examples/minitwit/'):
+        run('supervisorctl restart all')
+
+def install():
+    run('cd ~/flask/examples/minitwit;sh install.sh')
+
+def update_db():
+    with cd('~/flask/examples/minitwit/'):
+        run('openssl enc -des -d -a -in alancer_db_enc -out alancer.db')
 
 def free():
     run('free -mh')
