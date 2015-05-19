68ea1c0e4fb4a475cac2a080f3fdfbae125073e5
polo <txdywy@outlook.com>
2015-05-19 09:20:23 +0000

    add redis and client

---
+++ examples/minitwit/cache.py
@@ -0,0 +1,36 @@
+from redis import Redis
+import cPickle as pickle
+try:
+    from config import REDIS_CACHE, REDIS_PORT
+except:
+    print '==============redis config not found============'
+    REDIS_CACHE = REDIS_PORT = None
+if REDIS_CACHE:
+    rcache = Redis(host=REDIS_CACHE, port=REDIS_PORT)
+else:
+    rcache = None
+
+def dp(value):
+    return pickle.dumps(value)
+
+def ld(value):
+    if value is None:
+        return None
+    else:
+        return pickle.loads(value)
+
+def set(key, value, timeout=None):
+    v = dp(value)
+    if timeout:
+        rcache.set(key, v, timeout)
+    else:
+        rcache.set(key, v)
+    return True
+
+def get(key):
+    r = rcache.get(key)
+    return ld(r)
+
+def delete(key):
+    return rcache.delete(key)
+
