92d71842073e0a9dbba841a99fb6456f2db7dad1
Yi Wei <genius.ron@gmail.com>
2015-05-20 13:27:36 +0800

    add dns and vpn to fab

--- examples/minitwit/fabfile.py
+++ examples/minitwit/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import * 
 env.use_ssh_config = True
 #env.hosts = ['sushi', 'polo', 'noodle']
+
 def send(file):
     put(file, '~/')
 
@@ -9,6 +10,9 @@ def recv(file):
 
 def alancer():
     env.hosts = ['sushi', 'polo', 'noodle']
+
+def vpn():
+    env.hosts = ['tempura', 'ramen', 'donut']
 
 def pull():
     with cd('~/flask/examples/minitwit/'):
@@ -27,3 +31,17 @@ def update_db():
 
 def free():
     run('free -mh')
+
+def dns(url):
+    dns_server = ['', #local
+                  '8.8.8.8', #Google
+                  '8.8.4.4', #Google
+                  '114.114.114.114', #ChinaTele
+                  '4.2.2.2', #Legend
+                  '208.67.222.222', #OpenDNS
+                  '180.76.76.76', #Baidu
+                  '223.5.5.5', #Ali
+                  '223.6.6.6', #Ali
+                  ]
+    for dns in dns_server:
+        run('nslookup %s %s' % (url, dns))
