f363e98f87b8131576a8c302fea5ff086fb01b64
polo <txdywy@outlook.com>
2015-05-21 16:36:47 +0000

    fix sqs and add serori

--- examples/minitwit/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from config import SQS_AWS_ACCESS_KEY_ID, SQS_AWS_SECRET_ACCESS_KEY, SQS_REGION, SQS_QUEUE
-BROKER_URL = 'sqs://%s:%s@' % (SQS_AWS_ACCESS_KEY_ID, SQS_AWS_SECRET_ACCESS_KEY)
-app = Celery('tasks', broker=BROKER_URL)
-
-@app.task
-def add(x, y):
-    print '==================',x,y
-    return x + y
