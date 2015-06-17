248e201080df5528a6f063418f4f683653fdfcf6
Udon <txdywyos@outlook.com>
2015-06-17 08:04:46 +0000

    fix project edit

--- examples/minitwit/minitwit.py
+++ examples/minitwit/minitwit.py
@@ -622,10 +622,11 @@ def edit_project():
     project.client = request.form['client']
     project.desp = request.form['desp']
     project.image_url = request.form['image_url']
-    project.service = request.form['service']
-    project.location = request.form['location']
+    #project.service = request.form['service']
+    #project.location = request.form['location']
     project.incentive = request.form['incentive']
-    project.client_title = request.form['client_title']
+    #project.client_title = request.form['client_title']
+    project.valid_time = request.form['valid_time']
     flush(project)
     t = _('You have successfully updated your project')
     flash(t + ' [%s]' % project.title)
