0722fbabf2c80c0a9c4a26a6792957cb60eabe45
Udon <txdywyos@outlook.com>
2015-07-06 05:10:38 +0000

    fix test

--- examples/minitwit/views/test.py
+++ examples/minitwit/views/test.py
@@ -1,7 +1,7 @@
 from flask import (Blueprint, current_app, request, g, url_for, make_response,
                    render_template, redirect, jsonify)
 
-view = Blueprint('test', __name__)
+view = Blueprint('test', __name__, url_prefix='/test')
 
 @view.route('/haha')
 def haha():
