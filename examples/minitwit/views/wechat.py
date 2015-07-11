abee162644c09b73b03e6ac47f2ce47b8c251e3c
Udon <txdywyos@outlook.com>
2015-07-11 15:26:00 +0000

    add cache to wx view

--- examples/minitwit/views/wechat.py
+++ examples/minitwit/views/wechat.py
@@ -1,6 +1,7 @@
 from flask import (Blueprint, current_app, request, g, url_for, make_response,
                    render_template, redirect, jsonify)
 from models.model_wechat import *
+import cache as cachewx
 import cron
 
 view = Blueprint('wechat', __name__, url_prefix='/wechat')
@@ -8,10 +9,15 @@ view = Blueprint('wechat', __name__, url_prefix='/wechat')
 @view.route('/share')
 def share():
     key = request.args.get('k')
+    c = cachewx.get('VIEW_WX_SHARE_%s' % key)
+    if c:
+        title, abstract, pic_url = c
+        return render_template('wechat/share.html', title=title, abstract=abstract, pic_url=pic_url)
     ws = WechatShare.query.filter_by(key=key).first()
     print '==========', ws.data
     ab = ws.data['abstract']
     abstract = ab.split(' ')
+    title = ws.data['title']
     pic_url = ws.data.get('lc_url')
     if not pic_url:
        pic_url = ws.data['pic_url']
@@ -21,6 +27,9 @@ def share():
        else:
            ws.data['lc_url'] = pic_url
            flush(ws)
-    return render_template('wechat/share.html', title=ws.data['title'], abstract=abstract, pic_url=pic_url)
+           cachewx.set('VIEW_WX_SHARE_%s' % key, (title, abstract, pic_url), 60 * 60)
+    else:
+        cachewx.set('VIEW_WX_SHARE_%s' % key, (title, abstract, pic_url), 60 * 60)
+    return render_template('wechat/share.html', title=title, abstract=abstract, pic_url=pic_url)
 
 
