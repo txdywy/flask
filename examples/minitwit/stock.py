38545599e9584a5fc833d7bb78ee088c24303d07
Yi Wei <genius.ron@gmail.com>
2016-02-04 18:00:54 +0800

    fix format

--- examples/minitwit/stock.py
+++ examples/minitwit/stock.py
@@ -22,7 +22,7 @@ def get_us_stock():
     r = requests.get(SINA_STOCK_URL % ','.join(['gb_' + i for i in US_STOCK.values()])).text.strip()
     r = r.split(';')[:-1]
     r = [i.split('"')[1].split(',')[:] for i in r]
-    r = ['%s: $%s \n[$%s+$%s] \n[$%s-$%s]\n' % (i[0], i[1], i[7], _diff(i[1], i[7]), i[6], _diff(i[6], i[1])) for i in r]
+    r = ['%s: $%s \n%s+%s \n%s-%s\n' % (i[0], i[1], i[7], _diff(i[1], i[7]), i[6], _diff(i[6], i[1])) for i in r]
     r = '\n'.join(r)
     return r + '\n' + str(datetime.datetime.now())[:19]
 
@@ -31,7 +31,7 @@ def get_cn_stock():
     r = requests.get(SINA_STOCK_URL % ','.join(CN_STOCK.values())).text.strip()
     r = r.split(';')[:-1]
     r = [i.split('"')[1].split(',')[:] for i in r]
-    r = ['%s: %s \n[%s+%s] \n[%s-%s]\n' % (i[0], i[3], i[5], _diff(i[3], i[5]), i[4], _diff(i[4], i[5])) for i in r]
+    r = ['%s: %s \n%s+%s \n%s-%s\n' % (i[0], i[3], i[5], _diff(i[3], i[5]), i[4], _diff(i[4], i[5])) for i in r]
     r = '\n'.join(r)
     return r + '\n' + str(datetime.datetime.now())[:19]
 
