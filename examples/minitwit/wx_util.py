c1b18a3e18d12ed5171456499c9470aaba865e6f
Yi Wei <genius.ron@gmail.com>
2016-02-06 16:12:10 +0800

    fix cn

--- examples/minitwit/wx_util.py
+++ examples/minitwit/wx_util.py
@@ -311,7 +311,7 @@ def reply(data):
         result = cs
     stock_url = check_stock_graph(content)
     if stock_url:
-        body = stock.get_one_us_stock(content[1:])
+        body = stock.get_one_stock(content[1:])
         response = make_response(WX_TEMPLATE_IMG_TEXT % (user_name_from, user_name_to, str(int(time.time())),     content, body, stock_url, stock_url))
         response.content_type = 'application/xml'
         return response
