479afa5d0ca88c12d9d1be447c4e39a21fdfa861
mac <geniusron@gmail.com>
2016-02-12 22:56:27 +0800

    up base

--- examples/minitwit/stock.py
+++ examples/minitwit/stock.py
@@ -25,15 +25,34 @@ CN_STOCK = {'海天': 'sh603288',
             '东阿': 'sz000423',
             '海欣': 'sz002702',
             }
+            
+US_BASE = { 'fb': 99,
+            'msci': 63,
+            'jd': 22.3,
+            'app': 93,
+            'tw': 14.3,
+            'ms': 48,
+            'g': 670,
+            'amz': 475,
+            'tsla': 141,
+            'ali' : 60,
+            'net': 80,
+            'ko': 41.9,
+            'dis': 88,
+            'data': 35,
+          }
 
 SINA_STOCK_URL = 'http://hq.sinajs.cn/list=%s'
 
 
 def get_us_stock():
-    r = requests.get(SINA_STOCK_URL % ','.join(['gb_' + i for i in US_STOCK.values()])).text.strip()
+    r = requests.get(SINA_STOCK_URL % ','.join(['gb_' + US_STOCK[i] for i in US_STOCK])).text.strip()
     r = r.split(';')[:-1]
     r = [i.split('"')[1].split(',')[:] for i in r]
     r = ['%s: \n#[$%s]\n#%s+%s \n#%s-%s\n*[$%s] %s\n*%s+%s \n*%s-%s\n' % (i[0], i[1], i[7], _diff(i[1], i[7]), i[6], _diff(i[6], i[1]), i[21], _diff_sym(i[21], i[1]), i[7], _diff(i[21], i[7]), i[6], _diff(i[6], i[21])) for i in r]
+    b = [US_BASE[v] for k in US_BASE]
+    r = zip (r, b)
+    r = [a + '\nBase: [%s]' % b for a, b in r]
     r = '\n'.join(r)
     return r + '\n' + str(datetime.datetime.now())[:19] + '\n' + '#盘内/终\n*盘前/后'.decode('utf8')
 
@@ -43,7 +62,7 @@ def get_one_us_stock(k):
     r = r.split('"')[1].split(',')[:]
     i = r
     r = '%s: \n#[$%s]\n#%s+%s \n#%s-%s\n*[$%s] %s\n*%s+%s \n*%s-%s\n' % (i[0], i[1], i[7], _diff(i[1], i[7]),     i[6], _diff(i[6], i[1]), i[21], _diff_sym(i[21], i[1]), i[7], _diff(i[21], i[7]), i[6], _diff(i[6], i[21]))
-    return r + '\n' + str(datetime.datetime.now())[:19] + '\n' + '#盘内/终\n*盘前/后'.decode('utf8')
+    return r + '\n' + 'Base: ' + US_BASE[k] + '\n' + str(datetime.datetime.now())[:19] + '\n' + '#盘内/终\n*盘前/后'.decode('utf8')
 
 
 
