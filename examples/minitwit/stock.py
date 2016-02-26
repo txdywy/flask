a91329ee2c6535c3c4070b80036863872e68f349
Sushi <txdywyjp@outlook.com>
2016-02-26 02:53:30 +0000

    up st

--- examples/minitwit/stock.py
+++ examples/minitwit/stock.py
@@ -83,7 +83,7 @@ def get_cn_stock():
     r = [i.split('"')[1].split(',')[:] for i in r]
     r = ['%s: \n%s\n%s+%s \n%s-%s\n' % (i[0], i[3], i[5], _diff(i[3], i[5]), i[4], _diff(i[4], i[3])) for i in r]
     r = '\n'.join(r)
-    return r + '\n' + str(datetime.datetime.now())[:19] + '\n[Base:99254/103580]'
+    return r + '\n' + str(datetime.datetime.now())[:19] + '\n[Base:99254/118772.5]'
 
 
 def get_one_cn_stock(k):
