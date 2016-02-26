260c2afeb517a7a7ac3290e2a5dd92372ebb36f0
Udon <txdywyos@outlook.com>
2016-02-26 07:44:10 +0000

    up wx server

--- examples/minitwit/wxbot.py
+++ examples/minitwit/wxbot.py
@@ -38,7 +38,7 @@ MAX_GROUP_NUM = 35  # 每组人数
 INTERFACE_CALLING_INTERVAL = 20  # 接口调用时间间隔, 间隔太短容易出现"操作太频繁", 会被限制操作半小时左右
 MAX_PROGRESS_LEN = 50
 
-SERVER_QR_PATH = os.path.join(os.getcwd(), 'www/qrcode.jpg')
+SERVER_QR_PATH = os.path.join(os.getcwd(), 'static/qrcode.jpg')
 QRImagePath = os.path.join(os.getcwd(), 'qrcode.jpg')
 
 tip = 0
@@ -224,8 +224,11 @@ def showQRImage():
 
     tip = 1
 
+    global IS_SERVER
+    if sys.platform.find('linux') >= 0:
+        IS_SERVER = True
     if IS_SERVER:
-        with open(SERVER_QR_PATH, 'wb') as f
+        with open(SERVER_QR_PATH, 'wb') as f:
             f.write(response.read())
         print('请扫码二维码登录,地址 http://alancer.ml/qrcode.jpg')
         
@@ -679,7 +682,10 @@ def main(server=False):
     while waitForLogin() != '200':
         pass
 
-    os.remove(QRImagePath)
+    if not IS_SERVER:
+        os.remove(QRImagePath)
+    else:
+        os.remove(SERVER_QR_PATH)
 
     if not login():
         print('登录失败')
