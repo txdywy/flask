46f4f80a43956784829f97e7d31f3d7a3f6f17ec
Udon <txdywyos@outlook.com>
2016-03-03 14:39:23 +0000

    add more headers

--- examples/minitwit/nti.py
+++ examples/minitwit/nti.py
@@ -10,11 +10,18 @@ pxs = Proxy.query.filter_by(active=1).all()
 pxs = [p for p in pxs if 'sock' not in p.anonymity]
 print 'Valid http proxy: [%s]' % len(pxs)
 
-def geti(id=15):
-    px = pxs[randint(0, len(pxs)-1)]
+def geti(id=15, px=None):
+    #px = pxs[randint(0, len(pxs)-1)]
     headers = {'Referer': 'http://events.chncpa.org/wmx2016/mobile/pages/jmpx.php', 
                'X-Requested-With': 'XMLHttpRequest', 
-               'User-Agent': fake.user_agent()} 
+               'User-Agent': fake.user_agent(),
+               'Accept': 'application/json, text/javascript, */*; q=0.01',
+               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6',
+               'Cache-Control': 'no-cache',
+               'Host': 'events.chncpa.org',
+               'Pragma': 'no-cache', 
+               } 
     now = datetime.datetime.now()
     dtime = str(now)[:19]
     dtime = urllib.quote_plus(dtime)
