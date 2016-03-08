ca9890c26d30edb1f0a986e3cabc1eeaea05a2bb
Yi Wei <genius.ron@gmail.com>
2016-03-08 13:31:40 +0800

    fix

--- examples/minitwit/stock.py
+++ examples/minitwit/stock.py
@@ -115,7 +115,7 @@ def get_cn_stock():
     r = [i.split('"')[1].split(',')[:] for i in r]
     r = ['%s: \n%s\n%s+%s \n%s-%s\n' % (i[0], i[3], i[5], _diff(i[3], i[5]), i[4], _diff(i[4], i[3])) for i in r]
     r = '\n'.join(r)
-    return r + '\n' + str(datetime.datetime.now())[:19] + '\n[B:%s/%s][%+d]' % (CN_CASH, CN_BASE, US_CASH-US_BASE)
+    return r + '\n' + str(datetime.datetime.now())[:19] + '\n[B:%s/%s][%+d]' % (CN_CASH, CN_BASE, CN_CASH-CN_BASE)
 
 
 def get_one_cn_stock(k):
