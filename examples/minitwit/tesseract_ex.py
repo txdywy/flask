e311fa0c5ace24c792af6955e1f43b57f9c273b5
Udon <txdywyos@outlook.com>
2016-03-21 08:32:31 +0000

    add stackoverflow

--- examples/minitwit/tesseract_ex.py
+++ examples/minitwit/tesseract_ex.py
@@ -1,4 +1,7 @@
+#http://stackoverflow.com/questions/2363490/limit-characters-tesseract-is-looking-for
 import pytesseract
 from PIL import Image
+print pytesseract.image_to_string(Image.open('Captcha_gcjwazwmwn.jpg'))
 print pytesseract.image_to_string(Image.open('Captcha_qrdiqokdiw.jpg'))
+print pytesseract.image_to_string(Image.open('Captcha_mobwoaprwv.jpg'))
 
