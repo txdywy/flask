2c98763757f4de247f47b69e835dded62c1392b3
Yi Wei <genius.ron@gmail.com>
2016-03-23 17:20:51 +0800

    fix cookie

--- examples/minitwit/wxp.py
+++ examples/minitwit/wxp.py
@@ -41,7 +41,7 @@ def p(openid="o7VbEjuienGcLf33ZQ-V8bk0g67Q"):
             }
     headers = {"X-Requested-With": "XMLHttpRequest",
                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15 MicroMessenger/6.3.15 NetType/WIFI Language/zh_CN",
-               "Cookie": "Hm_lpvt_50e8608aef835699234de1e805173bbd=1458712338{openid}; Hm_lvt_50e8608aef835699234de1e805173bbd=1458712333; PHPSESSID=9k1rr2jk43qict1725qpsj5fh3; c_4753=o8qVGuD1L07_kBZ5IhTWj7bhxMnE; vipvopenid_83214_4753={openid}; vipvoteopenid_83214_4753={openid}".format(openid=openid),
+               "Cookie": "Hm_lpvt_50e8608aef835699234de1e805173bbd=1458712338; Hm_lvt_50e8608aef835699234de1e805173bbd=1458712333; PHPSESSID=9k1rr2jk43qict1725qpsj5fh3; c_4753=o8qVGuD1L07_kBZ5IhTWj7bhxMnE; vipvopenid_83214_4753={openid}; vipvoteopenid_83214_4753={openid}".format(openid=openid),
                "Accept": "application/json, text/javascript, */*; q=0.01",
                "Accept-Encoding": "gzip, deflate",
                "Accept-Language": "zh-cn",
