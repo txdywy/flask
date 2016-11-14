0a7e1b79827c5558460ed02605209cb2ea0ef67c
natto <txdywy05@yahoo.com>
2016-11-14 02:42:13 +0000

    add bj time to smash

--- examples/minitwit/smash.py
+++ examples/minitwit/smash.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import requests, sys, json
 from pprint import pprint
+import datetime
+import pytz
 try:
     import qy_util
 except:
     pass
+tz = pytz.timezone('Asia/Shanghai')
 bid = 'iUo0KQj6R2ghgZoObEXAxezvdHYzxwpZzwq7tI0Upu0Y3dXCP1GSkPP2IrQpZ/pICD2KISz0FC5jS4eDb57qBg=='
 headers = {
     'Host': 'api.smash.athinkingape.com',
@@ -30,8 +33,9 @@ def collect():
     pprint(o)
     resources_gained = str(o['resources_gained']['1'])
     print resources_gained
+    now = datetime.datetime.now(tz)
     try:
-        qy_util.post('SMASH自动采集金币:' + resources_gained, touser=['txdywy'])
+        qy_util.post('SMASH自动采集金币:' + resources_gained + '\n北京时间:' + str(now)[:19], touser=['txdywy'])
     except:
         print '没有微信推送'
     print 'SMASH自动采集金币:' + resources_gained
