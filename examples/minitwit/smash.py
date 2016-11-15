acaf6736d81c422d80607b15f65e1389e66cf4b5
natto <txdywy05@yahoo.com>
2016-11-15 10:05:37 +0000

    add energy print

--- examples/minitwit/smash.py
+++ examples/minitwit/smash.py
@@ -61,6 +61,7 @@ def target():
         return 0 
     target_user_id, target_type, energy_cost, target_user = o['pvp_targets'][2]['target_user']['user_id'], o['pvp_targets'][2]['target_type'], o['pvp_targets'][2]['energy_cost'], o['pvp_targets'][2]['target_user']
     energy = o['game_user']['energy']
+    print '================🔋', energy
     energy = 100 # api can not get accurate value, currently
     if int(energy) < int(energy_cost):
         print '能量不足以战斗🔋 [%s/%s]' % (int(energy), int(energy_cost))
