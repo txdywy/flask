837762883dbeec977739e1f7126b33d1cac4b7af
Wei Yi <weiyi@papayamobile.com>
2016-12-19 15:01:08 +0800

    add cloak

---
+++ examples/minitwit/cloak.sh
@@ -0,0 +1,4 @@
+curl -H "User-Agent: Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" -x socks5://73.179.134.109:37049 -L -D - "http://s3.amazonaws.com/ex11zjzw9o/695494b/redirect.html" -vv
+
+
+curl 'http://app.cloaker.io/go' -H 'Pragma: no-cache' -H 'Origin: http://s3.amazonaws.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Referer: http://s3.amazonaws.com/ex11zjzw9o/695494b/redirect.html' -H 'Connection: keep-alive' --data 'id=584aa74507db9d03d1367851&type=js&q=%2Fex11zjzw9o%2F695494b%2Fredirect.html' -x socks5://96.29.145.67:45554 -vv
