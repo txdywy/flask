a06b39d801d4bdcce72f1ead27108cb033ae2761
mac polo <geniusron@gmail.com>
2016-12-19 21:28:37 +0800

    add socks+ff

--- examples/minitwit/sele.py
+++ examples/minitwit/sele.py
@@ -35,3 +35,18 @@ driver.get("http://wtfismyip.com/text")
 print driver.current_url
 print driver.page_source
 driver.quit()
+
+
+
+from selenium import webdriver
+profile = webdriver.FirefoxProfile()
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 1080)
+driver = webdriver.Firefox(profile)
+driver.set_window_size(1120, 550)
+driver.get("http://wtfismyip.com/text")
+print driver.current_url
+print driver.page_source
+driver.quit()
+
