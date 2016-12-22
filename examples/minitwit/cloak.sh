131b4f236f24ebcd1483f8f4badd506705133f2e
mac polo <geniusron@gmail.com>
2016-12-22 20:55:50 +0800

    add more ex

--- examples/minitwit/cloak.sh
+++ examples/minitwit/cloak.sh
@@ -2,3 +2,11 @@ curl -H "User-Agent: Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/I
 
 
 curl 'http://app.cloaker.io/go' -H 'Pragma: no-cache' -H 'Origin: http://s3.amazonaws.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Referer: http://s3.amazonaws.com/ex11zjzw9o/695494b/redirect.html' -H 'Connection: keep-alive' --data 'id=584aa74507db9d03d1367851&type=js&q=%2Fex11zjzw9o%2F695494b%2Fredirect.html' -x socks5://96.29.145.67:45554 -vv
+
+
+curl -L 'http://veskron.com/brad-pitt-speaks-out?a=lej3nf-d' -H 'Pragma: no-cache' -H 'Origin: http://s3.amazonaws.com' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -x socks5://162.220.253.175:45554 -I
+
+
+
+
+curl -L 'http://wtfismyip.com/json' -x socks5://162.220.253.175:45554
