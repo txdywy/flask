8bf62b7e011031eb3eebc5fe944fcf428753dc06
Wei Yi <weiyi@papayamobile.com>
2016-12-23 18:22:50 +0800

    ex ipsearch ps

--- examples/minitwit/ip.py
+++ examples/minitwit/ip.py
@@ -68,9 +68,6 @@ def cloak(u='http://goo.gl/oI4ehT', pxy=None, c='US', p=False):
     ip, port = pxy[1], pxy[2]
     print 
     print
-    ipsearch(ip)
-    print
-    print
     pprint(pxy)
     print
     print
@@ -79,6 +76,9 @@ def cloak(u='http://goo.gl/oI4ehT', pxy=None, c='US', p=False):
     print
     s = "curl -H 'pragma: no-cache' -H 'accept-encoding: gzip, deflate, sdch, br' -H 'accept-language: en-US,en;q=0.8,zh-CN;q=0.6' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Mobile Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'cache-control: no-cache' -H 'authority: goo.gl' -H 'referer: https://m.facebook.com/' --compressed -L '{u}' -x socks5://{ip}:{port} -I".format(u=u, ip=ip, port=port)
     print s
+    print
+    print
+    ipsearch(ip)
 
 
 def _spc(s):
