f47328c539445a50b89a51e28e607cea615b38e7
mei <txdywy21@163.com>
2017-05-19 03:14:12 +0000

    new dance_db

--- examples/minitwit/dance_db_up.py
+++ examples/minitwit/dance_db_up.py
@@ -1,20 +1,7 @@
-new = ['http://m.qpic.cn/psb?/V11wKDiG23Nj7m/Ey4eMbXP92a8qUQmVpnKOQC01kSA95sQa6nW9PIgT44!/b/dBkBAAAAAAAA&ek=1&kp=1&pt=0&bo=wAP.BAAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/3OBYx1.USnYPSDbc*f4o90RmrDXsbthAbH7kwnkedlg!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=wAP.BAAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/1MbB6CksXGOnpeo*8p4hvfLeVYrVOULhhbTA1di4VLU!/b/dB8BAAAAAAAA&ek=1&kp=1&pt=0&bo=wAP.BAAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/ud.P5xCiMhAHcBG8xQOTJtYGP9Lh5honi6w.aBzIBC8!/b/dBcBAAAAAAAA&ek=1&kp=1&pt=0&bo=wAP.BAAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/hNmO7Ilh3Inx2sWjSUACo62LwFQGhYKYqW7vTu4LRqY!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=*gTAAwAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/8CcTNxjAxy*K2kv.0HOivstJUNSA8umlyr5MzpWpMWI!/b/dBcBAAAAAAAA&ek=1&kp=1&pt=0&bo=*gTAAwAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/LeDVWg1H5emz5uEnU6d*bhp4ZsIvxLt*RkQpougSqlk!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=*gTAAwAAAAARFxk!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/bpYvuKY1fbbcq5Und0CFdKxUzwtKwWeDD.yROl5pEwg!/b/dB8BAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/8eP3FT4NO1lmM0yjDE7.CIvWHTwtsfOVNRN1RP20JhE!/b/dBkBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/l85UzS*ApWkZJgm17La.LKXWK.iVW1H5BxuJsKbRTlM!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/8IsYE6QxkElZp5oaVNLKUQ.A9iXJ7z6.w.tjqhe3pYw!/b/dBcBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/AIBH.xiwiBBMeZRDu6K.8tLBEDA8BN4Bb1gJmuPOKdg!/b/dBkBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/6gsojWWshkldtswrI70nTfGeary1QmwwPeaxFG2QN4U!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-       'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/bqWcBFC.El*3ADdszCa6LnC7PEMKvWCOg6SHU4F5Owg!/b/dBgBAAAAAAAA&ek=1&kp=1&pt=0&bo=OASgBQAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-'http://m.qpic.cn/psb?/V11wKDiG23Nj7m/uyfSyWj2jr7QetvXDm3DqErC3jCO6QeSmJA254GFSHU!/b/dPYAAAAAAAAA&ek=1&kp=1&pt=0&bo=oAU4BAAAAAARF7k!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-'http://b265.photo.store.qq.com/psb?/V11wKDiG23Nj7m/9.exWjKnT7fPGsBJTCq4yCtei*2Cy8Ow8sYwnrOlDqM!/b/dAkBAAAAAAAA&ek=1&kp=1&pt=0&bo=oAU4BAAAAAAFF60!&tm=1495155600#sce=14-1-1&rf=v1_iph_sq_6.5.0_0_hdbm_t-4-2',
-]
+with open('dance_pic','r') as f:
+    new = f.readlines() 
+new = [i.strip('\n') for i in new]
+print new
 from models.model_mei import *
 for i in new:
     x = Dance(image_url=i)
