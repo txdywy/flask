4e5bc4407c34f4397c732f7f47520fd31102cc30
wei <txdywy22@163.com>
2017-07-25 08:28:51 +0000

    add hn

---
+++ examples/minitwit/hb/Util.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+
+import urllib
+import hashlib
+import hmac
+import base64
+
+#在此输入您的Key
+from config import ACCESS_KEY, SECRET_KEY
+
+HUOBI_SERVICE_API="https://api.huobi.com/apiv3"
+
+BUY = "buy"
+BUY_MARKET = "buy_market"
+CANCEL_ORDER = "cancel_order"
+ACCOUNT_INFO = "get_account_info"
+NEW_DEAL_ORDERS = "get_new_deal_orders"
+ORDER_ID_BY_TRADE_ID = "get_order_id_by_trade_id"
+GET_ORDERS = "get_orders"
+ORDER_INFO = "order_info"
+SELL = "sell"
+SELL_MARKET = "sell_market"
+
+def signature(params):
+    params = sorted(params.iteritems(), key=lambda d:d[0], reverse=False)
+    message = urllib.urlencode(params)
+    m = hashlib.md5()
+    m.update(message)
+    m.digest()
+    sig=m.hexdigest()
+    return sig
+
