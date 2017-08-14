576b409632d2708a9cfb6371262193e09292385d
Yi Wei <genius.ron@gmail.com>
2017-08-14 14:33:19 +0800

    add yw

---
+++ examples/minitwit/youthwant.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+with open('a.html', 'r') as f:
+    s = f.read()
+soup = BeautifulSoup(s)
+soup = soup.findAll("img", {"style": "max-width:600px"})
+r = [i.get('src') for i in soup]
+for u in r:
+    print u
+print '\n\n\n'
+
+
+
+hs = ['<img src="%s" width="640" class="alignnone size-medium" />' % u for u in r]
+
+for u in hs:
+    print u
+    print
+
