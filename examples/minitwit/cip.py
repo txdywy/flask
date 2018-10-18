4d74d9e0985d1cfc0b17ff183432056818b3d387
Yi Wei <genius.ron@gmail.com>
2018-10-18 09:57:04 +0800

    cip functional

--- examples/minitwit/cip.py
+++ examples/minitwit/cip.py
@@ -1,9 +1,15 @@
 import aiohttp
 import asyncio
-import uvloop
 import time
 import requests
 from bs4 import BeautifulSoup
+
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except:
+    print('No uvloop installed, use default asyncio loop')
+
 
 def page_proxy(r):
     #give me a response r and i give you all proxies entry data
@@ -19,7 +25,7 @@ def page_proxy(r):
         tds_list.append(td.text.strip())
     return tds_list
 
-headers = {
+PD_HEADERS = {
     'authority': 'www.proxydocker.com',
     'pragma': 'no-cache',
     'cache-control': 'no-cache',
@@ -31,34 +37,42 @@ headers = {
     'referer': 'https://www.proxydocker.com/en/socks5-list/',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6',
-    'cookie': '_ga=GA1.2.2096249051.1530681834; __tawkuuid=e::proxydocker.com::EvmzLc5o8dGEovH27iEhcbCsu79xTIpcgyAJm9k5j/r0eF0rIYhZkbYo4YYTrxeN::2; cookieconsent_status=dismiss; _gid=GA1.2.484481435.1534470821; REMEMBERME=TWFpbkJ1bmRsZVxFbnRpdHlcVXNlcjpkSGhrZVhkNToxNTM0NTU5MDg3Ojk5Y2E1NzgyZjExNmY2NWYzZWZiZTg0NWUyYzNiMmJlYjA5M2U1MWQ3N2IxM2Y5NjcwODZkZjRkN2Y2ZGZkNTc%3D; SERVERID31396=234013; PHPSESSID=120ed8919f1392088676e23138555d33; TawkConnectionTime=0',
+    'cookie': '_ga=GA1.2.587845913.1535014830; __tawkuuid=e::proxydocker.com::JWZ39h6IUYPaAAYuUYbjpiqGWfxMnQ5GPYDOJSuv1bjQTQrUaXpeajfwuZFRBueD::2; _gid=GA1.2.538432557.1539570325; cookieconsent_status=dismiss; REMEMBERME=TWFpbkJ1bmRsZVxFbnRpdHlcVXNlcjpkSGhrZVhkNToxNTM5ODM5OTgyOmQwN2Q3YmNlZDZjNGMxZjgxNmZhMGJhZDZjNDllNTY4OTZkNjg0ZTg4MjIzYWU0N2MxZjAyOGFjMzcxYTY1Mjg%3D; SERVERID31396=234013; PHPSESSID=6908453197842b2ef841260a1a9e5af9; _gat=1; TawkConnectionTime=0',
     'alexatoolbar-alx_ns_ph': 'AlexaToolbar/alx-4.0.3',
 }
 
-data = [
-  ('page', '1'),
-]
-#proxies = {
-#    'http': 'socks5://localhost:1080',
-#    'https': 'socks5://localhost:1080'
-#}
-r = requests.post('https://www.proxydocker.com/en/proxylist/search?port=All&type=HTTP&anonymity=All&country=United+States&city=All&state=All&need=All', data=data, headers=headers)
-x = page_proxy(r)
-x = [i for i in x if len(i) > 5]
-for i in x:
-    print(i)
 
-print(x)
 
-t0 = time.time()
-asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+def fetch_proxies(page='1'):
+    data = [
+      ('page', page),
+    ]
+    #proxies = {
+    #    'http': 'socks5://localhost:1080',
+    #    'https': 'socks5://localhost:1080'
+    #}
+    print('started dp fetch', time.strftime('%X'))
+    r = requests.post('https://www.proxydocker.com/en/proxylist/search?port=All&type=HTTP&anonymity=All&country=United+States&city=All&state=All&need=All', data=data, headers=PD_HEADERS)
+    print('done dp fetch', time.strftime('%X'))
+    proxies = page_proxy(r)
+    proxies = [i for i in proxies if len(i) > 5]
+    for i in proxies:
+        print(i)
+
+    print(proxies)
+    return proxies
+    
+
 
 async def fetch(session, url, proxy):
     headers = {'User-Agent:': 'curl/7.54.0'}
-    async with session.get(url, headers=headers, proxy=proxy, timeout=10) as response:
+    async with session.get(url, headers=headers, proxy=proxy, timeout=20) as response:
         return await response.text()
 
-async def coro(proxy):
+
+async def coro(proxy, t0=None):
+    if not t0:
+        t0 = time.time()
     async with aiohttp.ClientSession() as session:
         try:
             html = await fetch(session, 'http://ip-api.com', proxy)
@@ -71,24 +85,37 @@ async def coro(proxy):
 
         
 
-async def main(pxys):
-    coros = [coro('http://' + i) for i in pxys]
+async def main(pxys, t0=None):
+    coros = [coro('http://' + i, t0) for i in pxys]
     await asyncio.gather(*coros)
 
 
-pxys = ['46.63.45.88:37893',
-        '185.129.212.4:32054',
-        '5.141.81.80:55501',
-        '188.38.105.38:8080',
-        '158.255.26.221:41648',
-        '103.10.81.46:47077',
-        '118.175.176.137:43289',
-        '67.160.143.126:36523',
-        '97.72.176.78:87',
+TEST_PROXIES = ['46.63.45.88:37893',
+                '185.129.212.4:32054',
+                '5.141.81.80:55501',
+                '188.38.105.38:8080',
+                '158.255.26.221:41648',
+                '103.10.81.46:47077',
+                '118.175.176.137:43289',
+                '67.160.143.126:36523',
+                '97.72.176.78:87',
 ]  
-pxys = x
 
-print('started at', time.strftime('%X'))
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main(pxys))
-print('finished at', time.strftime('%X'))
+
+def fetch_and_poll(pages=1, test=False):
+    if test:
+        proxies = TEST_PROXIES
+    else:
+        proxies = []
+        for i in range(1, pages+1):
+            proxies += fetch_proxies(str(i))
+        print(proxies)
+    t0 = time.time()
+    print('started at', time.strftime('%X'))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(proxies, t0))
+    print('finished at', time.strftime('%X'))
+
+
+def run(pages=1):
+    fetch_and_poll(pages=pages)
